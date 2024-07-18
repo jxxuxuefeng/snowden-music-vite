3a4856da05d2827b8adda3d1c05c90b47a87c807
snowden.xu <snowden.xu@gmail.com>
2024-07-18 16:38:35 +0800

    feat: 接入tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div>
+    <div className="flex items-center">
+      <div className="text-3xl font-bold underline">Hello world!</div>
       <div>{count}</div>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
